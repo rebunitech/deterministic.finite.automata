0a0f424e5028de5f1885a869b45c78f69e5ceb34
Ashebir <83369330+Ashebir07@users.noreply.github.com>
2022-01-14 01:30:08 +0300

    Update dfa.py

--- rebuni/dfa.py
+++ rebuni/dfa.py
@@ -9,10 +9,10 @@
 * 	 1. Wendirad Demelash 			UGR/17757/11
 * 	 2. Getabalew Temesgen			UGR/17153/11
 * 	 3. Ashenafi Zenebe 			UGR/16813/11
-* 	 4. Ashebir Wondmeneh			UGR/XXXXX/11
-* 	 5. Yemisrach Mengstu			UGR/XXXXX/11
+* 	 4. Ashebir Wondmeneh			UGR/16809/11
+* 	 5. Yemisrach Mengstu			UGR/17801/11
 *
-*   Submited to: Ms. Fetlework
+*   Submited to: Ms. Fetlework kedir
 *   Submission date: 12/12/12
 *
 """
